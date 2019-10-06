aaaef1168dd9b4b4cb555a00e781f0710e3d80fb
Ashley To <ashleyto1130@gmail.com>
2019-10-06 17:17:22 -0400

    Initial Commit

---
+++ tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tippy
+//
+//  Created by Ashley Toe on 10/6/19.
+//  Copyright © 2019 Ashley Toe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
